2eb45c10cc68b9d1e2eb2c4eb956c4ad51ba83e1
maslovakat <maslova.ekaterina94@gmail.com>
2019-08-23 12:14:58 +0000

    first step

---
+++ react-project/src/index.js
@@ -0,0 +1,30 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// // import App from './App';
+// // import * as serviceWorker from './serviceWorker';
+
+// // ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<h1>ggggggg</h1>, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// // serviceWorker.unregister();
+
+
+// var mynew = document.createElement("p");
+// mynew.innerHTML = "trxtcfvyuubnkml,";
+
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+    ,document.getElementById("root")
+    )
